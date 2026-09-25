bab021f3c2514429c1b71c9535f00038f94f069c
giovannaferreiralopes <giovanna.ferreira.lopes@escola.pr.gov.br>
2026-09-25 12:59:42 +0000

    alt 1

---
+++ script.js
@@ -0,0 +1,20 @@
+function calcularMedia (){
+    const textoNota1 = document.getElementById("nota1").value;
+    const textoNota2 = document.getElementById("nota2").value;
+    const textoNota3 = document.getElementById("nota3").value;
+
+    const nota1 = Number(textoNota1);
+    const nota2 = Number(textoNota2);
+    const nota3 = Number(textoNota3);
+
+    const media = (nota1 + nota2 + nota3) / 3;
+
+    let situacao;
+
+    if (media >=7){
+        situacao = "APROVADO";
+        }
+        if (media >= 5){
+            situacao = "RECUPERAÇÂO";
+        }
+}
